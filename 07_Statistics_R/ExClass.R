0d1de046e614030d90e5458edbe7d3ba4ee8b3c4
Lourdes Rodriguez <lourodriguezmoreno@gmail.com>
2020-04-16 23:17:09 +0200

    Exercises

---
+++ 07_Statistics_R/ExClass.R
@@ -0,0 +1,35 @@
+# EJERCÍO 1
+# Tabla datos compuesta por las siguientes variables:
+# Sexo : V.a cualitativa Binaria
+# Edad: V.a Cuantitativa discreta
+# Estatura: V.A Cuantitativa contínua
+# Puntuación: V.A Cuantitativa discreta
+# Calificación: V.A. Cualitativa ordinaria
+
+# Variables cualitativa es mejor realizar un grafico tipo Pie
+# Variable cuantitativa mejor histograma
+
+# Diagramas BOXPLOT
+# Identifica datos atípicos ( son interesantes o están mal)
+# Analizar los QQplot
+# rango = factor de escala. Multiplicamos por ese valor y se alarga o complimen los bigotes
+
+# QQPLOT: Debemos especificar el tipo de distribución teórica de comparación
+# QQNORM: El tipo de distribución es una normal
+
+# ESTADISTICA INFERENCIAL PARAMÉTRICA
+# ajuste.nombre-distribució́n en R
+#mean          sd    
+#35.4575488    4.0878174 Valor que vamos a utilizar
+#( 0.4087817) ( 0.2890523) Error en esos valores
+
+# Bondad del ajuste: Nos indica si los parámetros son buenos para el modelo o no
+
+
+# P-valor = 0.1448
+# Nivel de confianza del 95%
+# alpha= 0.05
+# p-valor> alpha ACEPTAMOS Ho
+
+
+
